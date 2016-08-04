deb2bc2c0be1a61c904a7f08da135497a5ef24ec
ChrisXu1006 <rx37@cornell.edu>
2016-08-03 23:55:57 -0400

    add comments and error method to module class

--- module.py
+++ module.py
@@ -1,12 +1,24 @@
 from abc import ABCMeta, abstractmethod
 
+import sys
+
+# This the abstract class of hardware module, it only contains the 
+# information of name, parent and children hierarchy. Meanwhile, 
+# it only has some of general methods 
+
 class Module:
     __metaclass__ = ABCMeta
+    
+    # use the parent module and its own name to init
+    # if there is no parent module, parent argument is passed as NULL
 
-    def __init__():
-        self.name = ""
-        self.full_name = ""
-        self.parent = True
+    def __init__(parent, name):
+        self.name = name
+        if parent != NULL:
+            self.full_name = parent.full_name + '.' + name
+        else:
+            self.full_name = name
+        self.parent = parent
         self.child = []
    
     def _add_children(self, child):
@@ -15,3 +27,8 @@ class Module:
     def _display_children(self):
         for child in self.child:
             print "%s: %s\n" % (self.name, child.name)
+
+    def error(self):
+        print "Error from Module %s\n" % self.name
+        sys.exit(0)
+
